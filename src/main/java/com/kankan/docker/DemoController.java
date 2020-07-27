75542cd352e851672075e43c3b617b3b845e3fd4
何建文 <hejianwen@kankan.com>
2020-07-27 20:01:55 +0800

    注释ACM

--- src/main/java/com/kankan/docker/DemoController.java
+++ src/main/java/com/kankan/docker/DemoController.java
@@ -3,12 +3,12 @@ package com.kankan.docker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope
+//@RefreshScope
 public class DemoController {
 
     private static final Logger logger = LoggerFactory.getLogger(DockerDemoApplication.class);
